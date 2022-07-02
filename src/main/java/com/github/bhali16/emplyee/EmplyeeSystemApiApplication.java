b6f3300c947c983de3eae23c0d0130019a359cb8
Waqar Ahmad Khan <waqarbinmuhammad@gmail.com>
2022-07-02 23:45:47 +0500

    Created Spring App and Connected MySQL

---
+++ src/main/java/com/github/bhali16/emplyee/EmplyeeSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.bhali16.emplyee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmplyeeSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmplyeeSystemApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/bhali16/emplyee/EmplyeeSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bhali16.emplyee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmplyeeSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
